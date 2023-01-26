1f8d47d47a94cbbac709477598072e67f881c0c8
Rachel <rachel-barth@live.fr>
2023-01-26 20:17:40 +0100

    Corrections avant deploy

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './index.css'
 import Home from './pages/Home';
-import Quote from './pages/Quote';
-import Book from './pages/Book';
-import Movie from './pages/Movie';
+import About from './pages/About';
+import RentalSheet from './pages/RentalSheet';
 import Error from './components/Error';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Portfolio from './pages/Portfolio';
 
 
 const rootElement = document.getElementById('root');
@@ -19,17 +17,12 @@ const root = createRoot(rootElement);
 root.render(
   <Router>
     <Header/>
-    
     <Routes>
       <Route exact path='/kasa' element={<Home />} />
 
-      <Route path='/quote' element={<Quote />} />
+      <Route path='/about' element={<About />} />
 
-      <Route path='/book' element={<Book />} />
-
-      <Route path='/movie' element={<Movie />} />
-
-      <Route path='/portfolio' element={<Portfolio/>} />
+      <Route path='/rental/:id' element={ <RentalSheet /> } />
 
       <Route path='/*' to="/error_404" element={<Error/>} />
     </Routes>
