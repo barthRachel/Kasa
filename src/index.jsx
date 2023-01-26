5a78c8a9ba4d51c55e42f9c5652f6ae9803c618e
Rachel Barthélery <43150113+barthRachel@users.noreply.github.com>
2023-01-26 19:51:31 +0100

    Corrections avant deploy

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './index.css'
 import Home from './pages/Home';
-import About from './pages/About';
-import RentalSheet from './pages/RentalSheet';
+import Quote from './pages/Quote';
+import Book from './pages/Book';
+import Movie from './pages/Movie';
 import Error from './components/Error';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Portfolio from './pages/Portfolio';
 
 
 const rootElement = document.getElementById('root');
@@ -17,12 +19,17 @@ const root = createRoot(rootElement);
 root.render(
   <Router>
     <Header/>
+    
     <Routes>
-      <Route exact path='/' element={<Home />} />
+      <Route exact path='/panda-dev' element={<Home />} />
 
-      <Route path='/about' element={<About />} />
+      <Route path='/quote' element={<Quote />} />
 
-      <Route path='/rental/:id' element={ <RentalSheet /> } />
+      <Route path='/book' element={<Book />} />
+
+      <Route path='/movie' element={<Movie />} />
+
+      <Route path='/portfolio' element={<Portfolio/>} />
 
       <Route path='/*' to="/error_404" element={<Error/>} />
     </Routes>
